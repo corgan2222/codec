539ef7d45a9ce4d97fc31a64f1c712ec38840abe
Sway <sway@milesight.com>
2024-03-19 18:55:10 +0800

    feat: support encode(downlink data) and decode(downlink response data)
    - wt101
    - wt201
    - wt301
    - ts201
    - ws52x
    - uc100
    - uc300

--- vendors/milesight-iot/wt101/wt101-decoder.js
+++ vendors/milesight-iot/wt101/wt101-decoder.js
@@ -76,14 +76,14 @@ function milesight(bytes) {
             decoded.window_detection = bytes[i];
             i += 1;
         }
-        // MOTOR STORKE CALIBRATION RESULT
+        // MOTOR STROKE CALIBRATION RESULT
         else if (channel_id === 0x08 && channel_type === 0xe5) {
             decoded.motor_calibration_result = bytes[i];
             i += 1;
         }
         // MOTOR STROKE
         else if (channel_id === 0x09 && channel_type === 0x90) {
-            decoded.motor_storke = readUInt16LE(bytes.slice(i, i + 2));
+            decoded.motor_stroke = readUInt16LE(bytes.slice(i, i + 2));
             i += 2;
         }
         // FREEZE PROTECTION
@@ -91,10 +91,22 @@ function milesight(bytes) {
             decoded.freeze_protection = bytes[i];
             i += 1;
         }
-        // MOTOR CURRENT POSTION
+        // MOTOR CURRENT POSITION
         else if (channel_id === 0x0b && channel_type === 0x90) {
             decoded.motor_position = readUInt16LE(bytes.slice(i, i + 2));
             i += 2;
+        }
+        // DOWNLINK RESPONSE
+        else if (channel_id === 0xfe) {
+            result = handle_downlink_response(channel_type, bytes, i);
+            decoded = Object.assign(decoded, result.data);
+            i = result.offset;
+        }
+        // DOWNLINK RESPONSE
+        else if (channel_id === 0xfe) {
+            result = handle_downlink_response(channel_type, bytes, i);
+            decoded = Object.assign(decoded, result.data);
+            i = result.offset;
         } else {
             break;
         }
@@ -162,4 +174,246 @@ function readMotorCalibration(type) {
         default:
             return "unknown";
     }
+}
+
+function handle_downlink_response(channel_type, bytes, offset) {
+    var decoded = {};
+
+    switch (channel_type) {
+        case 0x17: // timezone
+            decoded.timezone = readInt16LE(bytes.slice(offset, offset + 2)) / 10;
+            offset += 2;
+            break;
+        case 0x4a: // sync_time
+            decoded.sync_time = 1;
+            offset += 1;
+            break;
+        case 0x8e: // report_interval
+            // ignore the first byte
+            decoded.report_interval = readUInt16LE(bytes.slice(offset + 1, offset + 3));
+            offset += 3;
+            break;
+        case 0x3b: // time_sync_enable
+            decoded.time_sync_enable = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xb3: // temperature_control(enable)
+            decoded.temperature_control = decoded.temperature_control || {};
+            decoded.temperature_control.enable = bytes[offset];
+            offset += 1;
+            break;
+        case 0xae: // temperature_control(mode)
+            decoded.temperature_control = decoded.temperature_control || {};
+            decoded.temperature_control.mode = bytes[offset];
+            offset += 1;
+            break;
+        case 0xab: // temperature_calibration(enable, temperature)
+            decoded.temperature_calibration = {};
+            decoded.temperature_calibration.enable = bytes[offset];
+            decoded.temperature_calibration.temperature = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            offset += 3;
+            break;
+        case 0xb1: // temperature_target, temperature_error
+            decoded.temperature_target = readInt8(bytes[offset]);
+            decoded.temperature_error = readUInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            offset += 3;
+            break;
+        case 0xac: // valve_control_algorithm
+            decoded.valve_control_algorithm = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xb0: // freeze_protection_config(enable, temperature)
+            decoded.freeze_protection_config = decoded.freeze_protection_config || {};
+            decoded.freeze_protection_config.enable = readUInt8(bytes[offset]);
+            decoded.freeze_protection_config.temperature = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            offset += 3;
+            break;
+        case 0xaf: // open_window_detection(enable, rate, time)
+            decoded.open_window_detection = decoded.open_window_detection || {};
+            decoded.open_window_detection.enable = readUInt8(bytes[offset]);
+            decoded.open_window_detection.temperature_threshold = readInt8(bytes[offset + 1]) / 10;
+            decoded.open_window_detection.time = readUInt16LE(bytes.slice(offset + 2, offset + 4));
+            offset += 4;
+            break;
+        case 0x57: // restore_open_window_detection
+            decoded.restore_open_window_detection = 1;
+            offset += 1;
+            break;
+        case 0xb4: // valve_opening
+            decoded.valve_opening = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xad: // valve_calibration
+            decoded.valve_calibration = 1;
+            offset += 1;
+            break;
+        case 0x25: // child_lock_config
+            decoded.child_lock_config = decoded.child_lock_config || {};
+            decoded.child_lock_config.enable = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        default:
+            throw new Error("unknown downlink response");
+    }
+
+    return { data: decoded, offset: offset };
+}
+
+if (!Object.assign) {
+    Object.defineProperty(Object, "assign", {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            "use strict";
+            if (target == null) {
+                // TypeError if undefined or null
+                throw new TypeError("Cannot convert first argument to object");
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                if (nextSource == null) {
+                    // Skip over if undefined or null
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                var keysArray = Object.keys(Object(nextSource));
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
+        },
+    });
+}
+
+function handle_downlink_response(channel_type, bytes, offset) {
+    var decoded = {};
+
+    switch (channel_type) {
+        case 0x17: // timezone
+            decoded.timezone = readInt16LE(bytes.slice(offset, offset + 2)) / 10;
+            offset += 2;
+            break;
+        case 0x4a: // sync_time
+            decoded.sync_time = 1;
+            offset += 1;
+            break;
+        case 0x8e: // report_interval
+            // ignore the first byte
+            decoded.report_interval = readUInt16LE(bytes.slice(offset + 1, offset + 3));
+            offset += 3;
+            break;
+        case 0x3b: // time_sync_enable
+            decoded.time_sync_enable = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xb3: // temperature_control(enable)
+            decoded.temperature_control = decoded.temperature_control || {};
+            decoded.temperature_control.enable = bytes[offset];
+            offset += 1;
+            break;
+        case 0xae: // temperature_control(mode)
+            decoded.temperature_control = decoded.temperature_control || {};
+            decoded.temperature_control.mode = bytes[offset];
+            offset += 1;
+            break;
+        case 0xab: // temperature_calibration(enable, temperature)
+            decoded.temperature_calibration = {};
+            decoded.temperature_calibration.enable = bytes[offset];
+            if (decoded.temperature_calibration.enable === 1) {
+                decoded.temperature_calibration.temperature = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            }
+            offset += 3;
+            break;
+        case 0xb1: // temperature_target, temperature_error
+            decoded.temperature_target = readInt8(bytes[offset]);
+            decoded.temperature_error = readUInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            offset += 3;
+            break;
+        case 0xac: // valve_control_algorithm
+            decoded.valve_control_algorithm = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xb0: // freeze_protection_config(enable, temperature)
+            decoded.freeze_protection_config = decoded.freeze_protection_config || {};
+            decoded.freeze_protection_config.enable = readUInt8(bytes[offset]);
+            if (decoded.freeze_protection_config.enable === 1) {
+                decoded.freeze_protection_config.temperature = readInt16LE(bytes.slice(offset + 1, offset + 3)) / 10;
+            }
+            offset += 3;
+            break;
+        case 0xaf: // open_window_detection(enable, rate, time)
+            decoded.open_window_detection = decoded.open_window_detection || {};
+            decoded.open_window_detection.enable = readUInt8(bytes[offset]);
+            if (decoded.open_window_detection.enable === 1) {
+                decoded.open_window_detection.temperature_threshold = readInt8(bytes[offset + 1]) / 10;
+                decoded.open_window_detection.time = readUInt16LE(bytes.slice(offset + 2, offset + 4));
+            }
+            offset += 4;
+            break;
+        case 0x57: // restore_open_window_detection
+            decoded.restore_open_window_detection = 1;
+            offset += 1;
+            break;
+        case 0xb4: // valve_opening
+            decoded.valve_opening = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        case 0xad: // valve_calibration
+            decoded.valve_calibration = 1;
+            offset += 1;
+            break;
+        case 0x25: // child_lock_config
+            decoded.child_lock_config = decoded.child_lock_config || {};
+            decoded.child_lock_config.enable = readUInt8(bytes[offset]);
+            offset += 1;
+            break;
+        default:
+            throw new Error("unknown downlink response");
+    }
+
+    return { data: decoded, offset: offset };
+}
+
+if (!Object.assign) {
+    Object.defineProperty(Object, "assign", {
+        enumerable: false,
+        configurable: true,
+        writable: true,
+        value: function (target) {
+            "use strict";
+            if (target == null) {
+                // TypeError if undefined or null
+                throw new TypeError("Cannot convert first argument to object");
+            }
+
+            var to = Object(target);
+            for (var i = 1; i < arguments.length; i++) {
+                var nextSource = arguments[i];
+                if (nextSource == null) {
+                    // Skip over if undefined or null
+                    continue;
+                }
+                nextSource = Object(nextSource);
+
+                var keysArray = Object.keys(Object(nextSource));
+                for (var nextIndex = 0, len = keysArray.length; nextIndex < len; nextIndex++) {
+                    var nextKey = keysArray[nextIndex];
+                    var desc = Object.getOwnPropertyDescriptor(nextSource, nextKey);
+                    if (desc !== undefined && desc.enumerable) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+            return to;
+        },
+    });
 }
